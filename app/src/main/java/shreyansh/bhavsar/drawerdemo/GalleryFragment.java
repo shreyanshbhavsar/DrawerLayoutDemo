7a57d04efadb760de15c07bc2a146fc0485eeef3
unknown <shreyansh.bhavsar@Tactbdnb8548.collaberaib.com>
2017-08-20 16:28:02 +0530

    Initial commit

---
+++ app/src/main/java/shreyansh/bhavsar/drawerdemo/GalleryFragment.java
@@ -0,0 +1,26 @@
+package shreyansh.bhavsar.drawerdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shreyansh.bhavsar on 8/20/2017.
+ */
+
+public class GalleryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.galfrg,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
